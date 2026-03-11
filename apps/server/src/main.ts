52e57ac80a73ed1d8c8f0c7e1f1afbbd6e593633
yuval.nachman01 <yuval.nachman01@post.runi.ac.il>
2026-03-11 16:35:42 +0200

    added swagger to server main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -6,12 +6,23 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Mandalat Halev Project API')
+    .setDescription('API for the Mandalat Halev Project')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
