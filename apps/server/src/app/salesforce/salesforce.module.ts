d2a3ee7b392dd2a234e33db0073dd68cfe472305
alon-nitzan <alon.nit@gmail.com>
2026-03-18 15:24:52 +0200

    Finished authenticate and general query to salesforce's server methods

--- apps/server/src/app/salesforce/salesforce.module.ts
+++ apps/server/src/app/salesforce/salesforce.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { SalesforceService } from './salesforce.service';
 
 @Module({
+  imports: [HttpModule],
   providers: [SalesforceService],
+  exports: [SalesforceService],
 })
 export class SalesforceModule {}
