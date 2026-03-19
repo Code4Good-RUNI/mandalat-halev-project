80384cfb30b3a5439bc1c35d75b94c884c9aa4bc
alon-nitzan <alon.nit@gmail.com>
2026-03-19 20:31:37 +0200

    added services templates: get user's profile, get future campaigns

--- apps/server/src/app/salesforce/salesforce.module.ts
+++ apps/server/src/app/salesforce/salesforce.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { SalesforceService } from './salesforce.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule],
   providers: [SalesforceService],
   exports: [SalesforceService],
 })
--- apps/server/src/app/salesforce/salesforce.service.ts
+++ apps/server/src/app/salesforce/salesforce.service.ts
@@ -5,8 +5,8 @@ import { firstValueFrom } from 'rxjs';
 import { isAxiosError } from 'axios';
 import {
   LoginRequestDto,
-  LoginResponseDto,
   UserProfileDto,
+  GetFutureCampaignDto,
 } from '@mandalat-halev-project/api-interfaces';
 
 @Injectable()
@@ -18,7 +18,11 @@ export class SalesforceService {
   constructor(
     private readonly httpService: HttpService,
     private configService: ConfigService,
-  ) {}
+  ) {
+    // ---------------------- test auth ----------------
+    //this.authenticate().catch(err => {//
+    //});
+  }
 
   // Private method to authenticate to Salesforce's server (Server-to-Server)
   private async authenticate(): Promise<void> {
@@ -50,9 +54,7 @@ export class SalesforceService {
       this.accessToken = data.access_token;
       this.instanceUrl = data.instance_url;
       this.logger.log('Salesforce authentication successful.');
-
     } catch (error) {
-
       // HTTP error
       if (isAxiosError(error)) {
         const errorData = error.response?.data;
@@ -105,9 +107,163 @@ export class SalesforceService {
     }
   }
 
-  // Public method to login
-  async login(data: LoginRequestDto): Promise<LoginResponseDto> {}
+  /**
+   * Authenticate User
+   * @param credentials - LoginRequestDto (phoneNumber, idNumber)
+   * @returns User's ID or null if not exists
+   */
+  async validateLogin(credentials: LoginRequestDto): Promise<number | null> {
+    const { phoneNumber, idNumber } = credentials;
 
-  // Public method to get user's profile by id number
-  async getUserProfile(id: number): Promise<UserProfileDto> {}
+    // sql query
+    const soql = `
+    SELECT External_ID__c 
+    FROM Contact 
+    WHERE (Phone = '${phoneNumber}' OR MobilePhone = '${phoneNumber}')
+    AND RegisteredID__c = '${idNumber}' 
+    LIMIT 1`;
+
+    const records = await this.query<any>(soql);
+
+    // login failed
+    if (records.length === 0) {
+      this.logger.warn(`Login attempt failed for ID: ${idNumber}`);
+      return null;
+    }
+    return Number(records[0].External_ID__c);
+  }
+
+  /**
+   * Get user's full profile by Salesforce user ID
+   * @param salesforceUserId - Salesforce user ID received when logged in
+   * @returns UserProfileDto or null if not found
+   */
+  async getUserProfile(
+    salesforceUserId: number,
+  ): Promise<UserProfileDto | null> {
+    // soql query
+    const soql = `SELECT External_ID__c, FirstName, LastName, Email, Phone,
+       RegisteredID__c, MailingStreet, CityName__c, Birthdate
+    FROM Contact WHERE External_ID__c = '${salesforceUserId}' LIMIT 1`;
+
+    try {
+      const records = await this.query<any>(soql);
+
+      if (records.length === 0) {
+        this.logger.warn(
+          `Profile not found for Salesforce User ID: ${salesforceUserId}`,
+        );
+        return null;
+      }
+
+      const raw = records[0];
+
+      // map to UserProfileDto
+      return {
+        salesforceUserId: Number(raw.External_ID__c),
+        firstName: raw.FirstName || '',
+        lastName: raw.LastName || '',
+        email: raw.Email || '',
+        phoneNumber: raw.Phone || '',
+        idNumber: raw.RegisteredID__c || '',
+        address: raw.MailingStreet || '',
+        city: raw.CityName__c || '',
+        birthDate: this.formatDateToIsraeli(raw.Birthdate), // convert if needed
+      };
+    } catch (error) {
+      this.logger.error(
+        `Error fetching profile for user ${salesforceUserId}`,
+        error,
+      );
+      throw error;
+    }
+  }
+
+  /**
+   * Convert date to Israeli format if needed
+   */
+  private formatDateToIsraeli(dateStr: string): string {
+    if (!dateStr) return '';
+    const [year, month, day] = dateStr.split('-');
+    return `${day}/${month}/${year}`;
+  }
+
+  /**
+   * Get user's future campaigns
+   * @param salesforceUserId - Salesforce user ID received when logged in
+   * @returns UserProfileDto or null if not found
+   */
+  async getFutureCampaigns(salesforceUserId: number,): Promise<GetFutureCampaignDto[]> {
+    // gets user ID in salesforce server
+    const contact = await this.query<any>(
+      `SELECT Id FROM Contact WHERE External_ID__c = '${salesforceUserId}' LIMIT 1`,
+    );
+
+    if (contact.length === 0) {
+      this.logger.warn(`User with External ID ${salesforceUserId} not found in Salesforce`,);
+      return [];
+    }
+    const contactId = contact[0].Id;
+
+    // SOQL query for relative date
+    const soql = `
+      SELECT
+        External_ID__c, Name, Description, StartDate, EndDate, IsActive,
+        ActivityLocation__c, Max_Participants__c, Image_URL__c,
+        (SELECT Status FROM CampaignMembers WHERE ContactId = '${contactId}' LIMIT 1)
+      FROM Campaign
+      WHERE StartDate >= TODAY AND IsActive = true
+    `;
+
+    const records = await this.query<any>(soql);
+
+    // map data to GetFutureCampaignDto array
+    return records.map((reg): GetFutureCampaignDto => {
+      const membership =
+        reg.CampaignMembers && reg.CampaignMembers.records
+          ? reg.CampaignMembers.records[0]
+          : null;
+
+      return {
+        // fields of CampaignDto
+        id: reg.External_ID__c ? Number(reg.External_ID__c) : 0,
+        name: reg.Name || '',
+        description: reg.Description || '',
+        imageUrl: reg.Image_URL__c || '',
+        startDate: this.formatDateToIsraeli(reg.StartDate),
+        endDate: this.formatDateToIsraeli(reg.EndDate),
+        durationInHours: this.calculateDuration(reg.StartDate, reg.EndDate),
+        locationAddress: reg.Location_Address__c || '',
+        locationCity: reg.Location_City__c || '',
+        numOfParticipants: reg.Max_Participants__c || 0,
+        numOfParticipantsRegistered: 0, // need to check if exist or need to calculate
+        isActive: !!reg.IsActive,
+
+        // fields of GetFutureCampaignDto
+        isRelevantToUser: true,
+        isUserRegistered: !!membership,
+        userApprovalStatus: this.mapStatusToApproval(membership?.Status),
+      };
+    });
+  }
+
+  /**
+   * Gets user approval status
+   */
+  private mapStatusToApproval(status: string) {
+    if (status === 'Confirmed') return 'approved';
+    if (status === 'Rejected') return 'rejected';
+    return 'pending';
+  }
+
+  /**
+   * Calculates duration
+   */
+  private calculateDuration(start: string, end: string): number {
+    if (!start || !end) return 0;
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    const diffInMs = endDate.getTime() - startDate.getTime();
+    return Math.max(0, Math.floor(diffInMs / (1000 * 60 * 60)));
+  }
 }
