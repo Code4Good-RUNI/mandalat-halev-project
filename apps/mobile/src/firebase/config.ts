960c16737769822a3ca34e8e2f1d19bf0ee93b10
yuval.nachman01 <yuval.nachman01@post.runi.ac.il>
2026-05-06 18:50:11 +0300

    added firebase config to mobile app

---
+++ apps/mobile/src/firebase/config.ts
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAjMYuzz_-NXgFyP2XslePuyaRga25dYAs",
+    authDomain: "mandalat-halev-app.firebaseapp.com",
+    projectId: "mandalat-halev-app",
+    storageBucket: "mandalat-halev-app.firebasestorage.app",
+    messagingSenderId: "742059784204",
+    appId: "1:742059784204:web:52219444a2f9e5fead9629",
+    measurementId: "G-QBCBFW0JG3"
+  };
