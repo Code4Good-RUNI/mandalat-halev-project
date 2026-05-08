8003af2ff24760ae8158a0c9d340b3860d40ff5e
Shira Maor <shira.maor@post.runi.ac.il>
2026-05-08 16:43:10 +0300

    integrate firebase admin and encrypt server environment variables

--- apps/server/src/app/client-app/auth/auth.controller.ts
+++ apps/server/src/app/client-app/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, UnauthorizedException, Headers } from '@nestjs/common';
 import { tsRestHandler, TsRestHandler } from '@ts-rest/nest';
 import { userContract } from '@mandalat-halev-project/api-interfaces';
-import * as admin from 'firebase-admin';
+import { AuthService } from './auth.service';
+
 
 @Controller()
 export class AuthController {
+
+  constructor(private readonly authService: AuthService) {}
   
   @TsRestHandler(userContract.auth.login)
   async executeLogin() {
@@ -28,23 +31,9 @@ export class AuthController {
 
       const token = authHeader.split(' ')[1];
 
-      try {
-        const decodedToken = await admin.auth().verifyIdToken(token);
-
-        if (body.phoneNumber === '0500000000') {
-          throw new UnauthorizedException('Invalid phone number or ID number');
-        }
-  
-        const salesforceUserId = '101'; 
-
-        await admin.auth().setCustomUserClaims(decodedToken.uid, {
-          salesforceUserId: salesforceUserId,
-        });
-
-        return { status: 200, body: { ok: true } };
-      } catch {
-        throw new UnauthorizedException('Invalid or expired Firebase token');
-      }
+      await this.authService.createSession(token);
+      
+      return { status: 200, body: { ok: true } };
     });
   }
 }
